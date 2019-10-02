e3f2040622a6c47a3b206aa71a28c2c95d1bac34
unknown <dmbujak3@gmail.com>
2019-10-02 17:31:58 -0400

    started lab 2

---
+++ Lab2/src/main.c
@@ -0,0 +1,338 @@
+/******************************************************************************
+  * @file    GPIO/GPIO_EXTI/Src/main.c
+  * @author  MCD Application Team
+  * @version V1.8.0
+  * @date    21-April-2017
+  * @brief   This example describes how to configure and use GPIOs through
+  *          the STM32L4xx HAL API.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT(c) 2017 STMicroelectronics</center></h2>
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/** @addtogroup STM32L4xx_HAL_Examples
+  * @{
+  */
+
+/** @addtogroup GPIO_EXTI
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+
+char lcd_buffer[6];    // LCD display buffer
+
+
+
+__IO HAL_StatusTypeDef Hal_status;  //HAL_ERROR, HAL_TIMEOUT, HAL_OK, of HAL_BUSY 
+uint16_t EE_status=0;
+uint16_t VirtAddVarTab[NB_OF_VAR] = {0x5555, 0x6666, 0x7777}; // the emulated EEPROM can save 3 varibles, at these three addresses.
+uint16_t EEREAD;  //to practice reading the BESTRESULT save in the EE, for EE read/write, require uint16_t type
+
+
+
+
+RNG_HandleTypeDef Rng_Handle;
+
+
+
+
+
+
+/* Private function prototypes -----------------------------------------------*/
+static void SystemClock_Config(void);
+static void Error_Handler(void);
+
+
+
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  /* STM32L4xx HAL library initialization:
+       - Configure the Flash prefetch
+       - Systick timer is configured by default as source of time base, but user 
+         can eventually implement his proper time base source (a general purpose 
+         timer for example or other time source), keeping in mind that Time base 
+         duration should be kept 1ms since PPP_TIMEOUT_VALUEs are defined and 
+         handled in milliseconds basis.
+       - Set NVIC Group Priority to 4
+       - Low Level Initialization
+     */
+	
+	HAL_Init();
+
+  /* Configure the system clock to 4 MHz */
+  SystemClock_Config();
+ 
+	HAL_InitTick(0x0000); //set the systick interrupt priority to the highest
+
+	BSP_LED_Init(LED4);
+	BSP_LED_Init(LED5);
+	BSP_LED_Off(LED4);
+
+	BSP_LCD_GLASS_Init();
+	
+
+	//BSP_LCD_GLASS_ScrollSentence((uint8_t*) "  mt3ta4 lab2 starter", 1, 200);
+	BSP_LCD_GLASS_DisplayString((uint8_t*)"MT3TA4");	
+	
+
+
+
+
+
+
+
+	
+//******************* use emulated EEPROM ====================================
+	//First, Unlock the Flash Program Erase controller 
+	HAL_FLASH_Unlock();
+		
+// EEPROM Init 
+	EE_status=EE_Init();
+	if(EE_status != HAL_OK)
+  {
+		Error_Handler();
+  }
+// then can write to or read from the emulated EEPROM
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//*********************use RNG ================================  
+Rng_Handle.Instance=RNG;  //Everytime declare a Handle, need to assign its Instance a base address. like the timer handles.... 													
+	
+	Hal_status=HAL_RNG_Init(&Rng_Handle);   //go to msp.c to see further low level initiation.
+	
+	if( Hal_status != HAL_OK)
+  {
+    Error_Handler();
+  }
+//then can use RNG
+	
+	
+
+  /* Infinite loop */
+  while (1)
+  {
+		
+
+	}
+}
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follows :
+  *            System Clock source            = MSI
+  *            SYSCLK(Hz)                     = 4000000
+  *            HCLK(Hz)                       = 4000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 1
+  *            APB2 Prescaler                 = 1
+  *            MSI Frequency(Hz)              = 4000000
+  *            Flash Latency(WS)              = 0
+  * @param  None
+  * @retval None
+  */
+
+
+void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+
+  // The following clock configuration sets the Clock configuration sets after System reset                
+  // It could be avoided but it is kept to illustrate the use of HAL_RCC_OscConfig and HAL_RCC_ClockConfig 
+  // and to be eventually adapted to new clock configuration                                               
+
+  // MSI is enabled after System reset at 4Mhz, PLL not used 
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6; // RCC_MSIRANGE_6 is for 4Mhz. _7 is for 8 Mhz, _9 is for 16..., _10 is for 24 Mhz, _11 for 48Hhz
+  RCC_OscInitStruct.MSICalibrationValue = RCC_MSICALIBRATION_DEFAULT;
+  
+//	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 40; 
+  RCC_OscInitStruct.PLL.PLLR = 2;  //2,4,6 or 8
+  RCC_OscInitStruct.PLL.PLLP = 7;   // or 17.
+  RCC_OscInitStruct.PLL.PLLQ = 4;   //2, 4,6, 0r 8  ===the clock for RNG will be 4Mhz *N /M/Q =40Mhz. which is nearly 48
+	
+	
+	
+	
+	if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    // Initialization Error 
+    while(1);
+  }
+
+  // Select MSI as system clock source and configure the HCLK, PCLK1 and PCLK2 clocks dividers 
+  // Set 0 Wait State flash latency for 4Mhz 
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    // Initialization Error 
+    while(1);
+  }
+
+  // The voltage scaling allows optimizing the power consumption when the device is
+  //   clocked below the maximum system frequency, to update the voltage scaling value
+  //   regarding system frequency refer to product datasheet.  
+
+  // Enable Power Control clock 
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  if(HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE2) != HAL_OK)
+  {
+    // Initialization Error 
+    while(1);
+  }
+
+  // Disable Power Control clock 
+  __HAL_RCC_PWR_CLK_DISABLE();
+}
+//after RCC configuration, for timmer 2---7, which are one APB1, the TIMxCLK from RCC is 4MHz
+
+
+
+
+
+/**
+  * @brief EXTI line detection callbacks
+  * @param GPIO_Pin: Specifies the pins connected EXTI line
+  * @retval None
+  */
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+  switch (GPIO_Pin) {
+			case GPIO_PIN_0: 		               //SELECT button					
+											
+						break;	
+			case GPIO_PIN_1:     //left button						
+						
+							break;
+			case GPIO_PIN_2:    //right button						  to play again.
+					
+							break;
+			case GPIO_PIN_3:    //up button							
+					
+							break;
+			
+			case GPIO_PIN_5:    //down button						
+					
+							break;
+			default://
+						//default
+						break;
+	  } 
+}
+
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)   //see  stm32lxx_hal_tim.c for different callback function names. 
+																															//for timer 3 , Timer 3 use update event initerrupt
+{
+	
+}
+
+
+void HAL_TIM_OC_DelayElapsedCallback(TIM_HandleTypeDef * htim) //see  stm32fxx_hal_tim.c for different callback function names. 
+{																																//for timer4 
+	
+
+		//clear the timer counter at the end of call back to avoid interrupt interval variation!  in stm32l4xx_hal_tim.c, the counter is not cleared after  OC interrupt
+		__HAL_TIM_SET_COUNTER(htim, 0x0000);   //this macro is defined in stm32l4xx_hal_tim.h
+
+}
+
+
+static void Error_Handler(void)
+{
+ 
+ 
+}
+
+
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(char *file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
